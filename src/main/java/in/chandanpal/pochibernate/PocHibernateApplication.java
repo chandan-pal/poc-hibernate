43b8de1a5b07155ee27213fbe0d22acab0039587
chandan-pal <chandanpal290190@gmail.com>
2020-03-19 12:10:57 +0530

    call hibernate api to save object in database

--- src/main/java/in/chandanpal/pochibernate/PocHibernateApplication.java
+++ src/main/java/in/chandanpal/pochibernate/PocHibernateApplication.java
@@ -1,13 +1,38 @@
 package in.chandanpal.pochibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import in.chandanpal.pochibernate.model.UserDetails;
 
 @SpringBootApplication
 public class PocHibernateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PocHibernateApplication.class, args);
+		
+		//tests
+		UserDetails user = new UserDetails();
+		user.setUserId(1);
+		user.setUserName("FirstUser");
+		
+		//create a session factory
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		
+		//get session
+		Session session = sessionFactory.openSession();
+		
+		//start transaction
+		session.beginTransaction();
+		
+		//do operation
+		session.save(user);
+		
+		//commit transaction
+		session.getTransaction().commit();
 	}
 
 }
