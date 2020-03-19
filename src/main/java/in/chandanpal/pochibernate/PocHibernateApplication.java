d87c6623aa54b41a9fd0d0d0a3ecb4bb11006f19
chandan-pal <chandanpal290190@gmail.com>
2020-03-19 21:51:26 +0530

    retrieve objects using session

--- src/main/java/in/chandanpal/pochibernate/PocHibernateApplication.java
+++ src/main/java/in/chandanpal/pochibernate/PocHibernateApplication.java
@@ -22,20 +22,41 @@ public class PocHibernateApplication {
 		//create a session factory
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		
-		//get session
-		Session session = sessionFactory.openSession();
+		//----------------------saving object with hibernate api---------------------------------------------------------------------------
 		
-		//start transaction
-		session.beginTransaction();
+			//get session
+			Session session = sessionFactory.openSession();
+			
+			//start transaction
+			session.beginTransaction();
+			
+			//do operation
+			session.save(user);
+			
+			//commit transaction
+			session.getTransaction().commit();
+			
+			//close session
+			session.close();
 		
-		//do operation
-		session.save(user);
 		
-		//commit transaction
-		session.getTransaction().commit();
+		//--------------------fething saved object with hibernate api---------------------------------------------------------------------------
 		
-		//close session
-		session.close();
+			//get session
+			session = sessionFactory.openSession();
+			
+			//start transaction
+			session.beginTransaction();
+			
+			//fetch object
+			UserDetails fetchedUser = (UserDetails) session.get(UserDetails.class, 1); //fetches the object which has the same primary key as the second argument
+			System.out.println("fetched user name=" + fetchedUser.getUserName());
+			
+			//commit transaction
+			session.getTransaction().commit();
+			
+			//close session
+			session.close();
 	}
 
 }
