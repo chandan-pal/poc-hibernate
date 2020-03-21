2cb2e7cd214fd08538e6828508b2574a15bdeffc
chandan-pal <chandanpal290190@gmail.com>
2020-03-21 19:42:33 +0530

    Transient, persistent and detached

--- src/main/java/in/chandanpal/pochibernate/PocHibernateApplication.java
+++ src/main/java/in/chandanpal/pochibernate/PocHibernateApplication.java
@@ -3,8 +3,13 @@ package in.chandanpal.pochibernate;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import in.chandanpal.pochibernate.model.User;
 
 @SpringBootApplication
 public class PocHibernateApplication {
@@ -14,7 +19,28 @@ public class PocHibernateApplication {
 		
 		//HibernateBasics.runHiberNateBasics();
 		
-		HibernateInheritance.runHiberNateInheritance();
+		//HibernateInheritance.runHiberNateInheritance();
+		
+		
+		//Transient, Persistent, Detached objects
+		User user = new User();
+		user.setUserName("New User");  //user object is transient
+		
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+		session.save(user); //user object is persistent, hibernate tracks for any changes in the object and saves that also in database.
+		
+		user.setUserName("Update user name"); //update not fired for this change
+		user.setUserName("New updated user name"); //update fired for this change
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		//user object is detached at this point
+		//hibernate is not tracking for changes in the object.
+		user.setUserName("update after session close"); //this change is not saved in the database
 	}
 
 }
